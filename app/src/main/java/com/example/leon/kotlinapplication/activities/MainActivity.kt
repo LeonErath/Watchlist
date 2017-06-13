9555a9590b7b3b3da3560286db58643cff65b289
Leon Erath <leon-erath@hotmail.de>
2017-06-13 07:28:20 +0200

    fixed toolbar

--- app/src/main/java/com/example/leon/kotlinapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/leon/kotlinapplication/activities/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.leon.kotlinapplication.activities
 
+import android.graphics.Color
 import com.example.leon.kotlinapplication.model.Movie
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
+import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -21,17 +23,19 @@ import com.example.leon.kotlinapplication.R.id.viewPager
 
 class MainActivity : AppCompatActivity() {
 
-    private var realm: Realm by Delegates.notNull()
-    private var eventListener: EventListener? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn = findViewById(R.id.button) as FloatingActionButton;
         val viewPager = findViewById(R.id.viewPager) as ViewPager
         val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, this)
         var tabLayout = findViewById(R.id.tab_layout) as TabLayout
+        var toolbar = findViewById(R.id.toolbar) as Toolbar
+
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(Color.WHITE)
 
         tabLayout.addTab(tabLayout.newTab().setText("Popular"));
         tabLayout.addTab(tabLayout.newTab().setText("Cinema"));
@@ -61,33 +65,14 @@ class MainActivity : AppCompatActivity() {
 
         viewPager.adapter = viewPagerAdapter
 
-        supportActionBar?.title = "Movies"
-
-        Realm.init(this)
-        realm = Realm.getDefaultInstance()
-
-
-        btn.setOnClickListener({
-            if (eventListener != null) {
-                eventListener!!.updateRecyclerView()
-            }
-        })
     }
 
 
-    fun nextActivity() {
-        //val intent = Intent(this, MenuActivity::class.java)
-        //startActivity(intent)
-    }
 
-    fun setOnEventListener(listener: EventListener) {
-        this.eventListener = listener
-    }
 
 
     override fun onDestroy() {
         super.onDestroy()
-        realm.close()
     }
 }
 
